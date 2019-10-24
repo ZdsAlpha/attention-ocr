77f92045a38cb30b4e7af6ed5f082b714a6f1161
ZdsAlpha <abzsoft999@gmail.com>
2019-10-25 03:27:05 +0500

    Auto update

--- aocr/util/dataset.py
+++ aocr/util/dataset.py
@@ -49,7 +49,7 @@ def generate(annotations_path, output_path, log_step=5000,
 
             feature = {}
             feature['image'] = _bytes_feature(img)
-            feature['label'] = _bytes_feature(b(label))
+            feature['label'] = _int64_feature(b(label))
             if save_filename:
                 feature['comment'] = _bytes_feature(b(img_path))
 
